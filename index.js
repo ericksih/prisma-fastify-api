16387bbc3325012b4600790b57de2859cbd47eb0
ericksih <erick.saputra08@gmail.com>
2021-11-18 18:07:34 +0700

    Update: users validation

--- index.js
+++ index.js
@@ -15,39 +15,6 @@ fastify.register(require("fastify-swagger"), {
 
 fastify.register(require("./routes/users")); // Start the server
 
-// // register user
-// fastify.post("/register", async (req, res) => {
-//   const { name, email } = req.body;
-
-//   if (!name || !email) {
-//     return res.status(400).send({
-//       error: "Please provide name and email", // 400 bad request error
-//     });
-//   }
-
-//   const userExists = await prisma.user.findUnique({
-//     // check if user exists
-//     where: {
-//       email,
-//     },
-//   });
-
-//   if (userExists) {
-//     return res.status(400).send({
-//       error: "User already exists",
-//     });
-//   }
-
-//   const user = await prisma.user.create({
-//     data: {
-//       name,
-//       email,
-//     },
-//   });
-
-//   return res.send(user);
-// });
-
 // Run the server!
 const start = async () => {
   try {
